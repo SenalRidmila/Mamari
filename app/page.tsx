2c513dbf352615cd67f571c99410b71c502af8ef
SenalRidmila <senalridmila2@gmail.com>
2026-09-05 00:56:43 +0530

    add favicon

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,13 @@ export const metadata: Metadata = {
   title: 'Mamari Foreign Employment Agency | Sri Lanka to Israel Jobs',
   description: 'Professional overseas recruitment agency in Sri Lanka providing highly skilled and semi-skilled manpower. Specializing in Israel construction sector jobs. Contact us today!',
   keywords: ['Foreign Employment Agency Sri Lanka', 'Jobs in Israel', 'Israel Construction Jobs', 'Manpower Agency Sri Lanka', 'Mamari', 'Kurundugaha Elpitiya', 'Overseas Recruitment'],
+  icons: {
+    icon: '/mamari.png', // මෙතනින් mamari.png එක favicon එක විදිහට set කළා
+  },
   openGraph: {
     title: 'Mamari Foreign Employment Agency',
     description: 'Connecting Talent | Creating Opportunities | Building Futures',
-    url: 'https://mamariforeign.lk', // Oyage domain eka methanata danna (passe)
+    url: 'https://mamariforeign.lk', 
     siteName: 'Mamari Foreign Employment Agency',
     locale: 'en_US',
     type: 'website',
