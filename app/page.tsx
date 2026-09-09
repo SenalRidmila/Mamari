9159893ca89080d3af45841b944a494ee9058db2
SenalRidmila <senalridmila2@gmail.com>
2026-09-09 17:17:11 +0530

    SEO update

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { Metadata } from 'next';
 import ClientComponent from './ClientComponent';
 
@@ -7,13 +6,16 @@ export const metadata: Metadata = {
   title: 'Mamari Foreign Employment Agency | Sri Lanka to Israel Jobs',
   description: 'Professional overseas recruitment agency in Sri Lanka providing highly skilled and semi-skilled manpower. Specializing in Israel construction sector jobs. Contact us today!',
   keywords: ['Foreign Employment Agency Sri Lanka', 'Jobs in Israel', 'Israel Construction Jobs', 'Manpower Agency Sri Lanka', 'Mamari', 'Kurundugaha Elpitiya', 'Overseas Recruitment'],
+  alternates: {
+    canonical: 'https://mamari.lk', // 
+  },
   icons: {
-    icon: '/mamari.png', // මෙතනින් mamari.png එක favicon එක විදිහට set කළා
+    icon: '/mamari.png', 
   },
   openGraph: {
     title: 'Mamari Foreign Employment Agency',
     description: 'Connecting Talent | Creating Opportunities | Building Futures',
-    url: 'https://mamariforeign.lk', 
+    url: 'https://mamari.lk', // 
     siteName: 'Mamari Foreign Employment Agency',
     locale: 'en_US',
     type: 'website',
@@ -21,8 +23,29 @@ export const metadata: Metadata = {
 };
 
 export default function Home() {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "EmploymentAgency",
+    "name": "Mamari Foreign Employment Agency",
+    "url": "https://mamari.lk",
+    "logo": "https://mamari.lk/mamari.png",
+    "description": "Professional overseas recruitment agency in Sri Lanka providing highly skilled and semi-skilled manpower to Israel.",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "No. 23, 2nd Floor, In Front of the Filling Station",
+      "addressLocality": "Kurundugaha, Elpitiya",
+      "addressCountry": "LK"
+    },
+    "telephone": "+94707157715"
+  };
+
   return (
     <main>
+      {/* Other components and content of the Home page */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
       <ClientComponent />
     </main>
   );
